4dd06e04eb31a080dcbccbc0f263c6146af03118
amansangal28 <50379218+amansangal28@users.noreply.github.com>
2019-08-04 23:12:04 +0530

    Add files via upload

---
+++ lfr.ino
@@ -0,0 +1,63 @@
+/* 
+ * Name - Akshay Srivastava
+ * Roll No- 12
+ * Name - Aman Sangal
+ * Roll No- 62
+ * Sec - A
+ * Project Title- Line Following Robot
+ * Date-19/11/18
+*/
+#define LS 2      // left sensor
+#define RS 1      // right sensor
+#define LM1 4     // left motor
+#define LM2 5     // left motor
+#define RM1 6     // right motor
+#define RM2 7     // right motor
+
+void setup()
+{
+  pinMode(LS, INPUT);
+  pinMode(RS, INPUT);
+  pinMode(LM1, OUTPUT);
+  pinMode(LM2, OUTPUT);
+  pinMode(RM1, OUTPUT);
+  pinMode(RM2, OUTPUT);
+}
+
+void loop()
+{
+  if(digitalRead(LS) && digitalRead(RS))     // Move Forward
+  {
+    digitalWrite(LM1, HIGH);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, HIGH);
+    digitalWrite(RM2, LOW);
+  }
+  
+  if(!(digitalRead(LS)) && digitalRead(RS))     // Turn right
+  {
+    digitalWrite(LM1, LOW);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, HIGH);
+    digitalWrite(RM2, LOW);
+  }
+  
+  if(digitalRead(LS) && !(digitalRead(RS)))     // Turn left
+  {
+    digitalWrite(LM1, HIGH);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, LOW);
+    digitalWrite(RM2, LOW);
+  }
+  
+  if(!(digitalRead(LS)) && !(digitalRead(RS)))     // Stop
+  {
+    digitalWrite(LM1, LOW);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, LOW);
+    digitalWrite(RM2, LOW);
+  }
+ Serial.println(LS);
+  Serial.println(" ");
+   Serial.println(RS);
+}
